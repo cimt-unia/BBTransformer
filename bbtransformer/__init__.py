19420647c1ddc7b580e65413195e54108bb8f8d0
ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>
2026-02-12 04:27:17 +0100

    Add run_analysis import to __init__.py
    Signed-off-by: ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>

--- bbtransformer/__init__.py
+++ bbtransformer/__init__.py
@@ -9,6 +9,7 @@ from .utils import save_model_weights, load_model_weights, load_roi_names
 # Trainer components
 from .trainer.loader import prepare_fmri_data
 from .trainer.train import train_model
+from .trainer.exe import run_analysis
 from .trainer.eval import evaluate_model, plot_results
 from .trainer.rank import (
     calculate_permutation_importance,
@@ -17,3 +18,4 @@ from .trainer.rank import (
 )
 from .trainer.tune import tune_hyperparameters
 from .trainer.pred import Diagnostic
+
